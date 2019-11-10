461d1f378ab8bc02969f8faa0d64536036aee6a1
veronicanardy <55004666+veronicanardy@users.noreply.github.com>
2019-11-10 19:19:54 -0300

    Add files via upload

---
+++ Atividade Prática 01/js/respostas_didatica.js
@@ -0,0 +1,45 @@
+   var cont=0;
+
+   function errado_primeiro(el){
+        el.style.backgroundColor = '#7D26CD';
+    }
+   function certo_primeiro(el){
+        el.style.backgroundColor = '#7D26CD';
+        cont++;
+    }
+
+   function errado_segundo(el){
+        el.style.backgroundColor = '#FF1493';
+    }
+   function certo_segundo(el){
+        el.style.backgroundColor = '#FF1493';
+        cont++;
+    }
+
+       function errado_terceiro(el){
+        el.style.backgroundColor = '#FF0000';
+    }
+   function certo_terceiro(el){
+        el.style.backgroundColor = '#FF0000';
+        cont++;
+    }
+
+       function errado_quarto(el){
+        el.style.backgroundColor = '#FFA500';
+    }
+   function certo_quarto(el){
+        el.style.backgroundColor = '#FFA500';
+        cont++;
+    }
+
+       function errado_quinto(el){
+        el.style.backgroundColor = '#7FFF00';
+    }
+   function certo_quinto(el){
+        el.style.backgroundColor = '#7FFF00';
+        cont++;
+    }
+
+    function calculo(){
+    	document.dados.res.value =  cont + " de 5 questões";
+    }
